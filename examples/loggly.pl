10e3be23d7dc8a127b76fb4af63e591710f45291
Simon Wistow <simon@fastly.com>
2013-04-24 15:25:07 -0700

    Add some examples that actually work and are useful for testing

--- examples/loggly.pl
+++ examples/loggly.pl
@@ -1,18 +1,16 @@
 #!perl -w
 ########
 #
-# Usage: papertrail.pl <papertrail port> <msg> [ssl]
+# Usage: loggly.pl <loggly port> <msg> [ssl]
 #
 ########
 use strict;
 use warnings;
-use lib qw(../lib);
 use Log::Syslog::Fast qw(:all);
 use Log::Syslog::Fast::PP;
 use Sys::Hostname qw(hostname);
 use IO::Socket::SSL qw(debug4);
 use FindBin qw($Bin);
-
 
 my $port   = shift;
 my $msg    = shift || die "You must pass in a message\n";
@@ -21,15 +19,14 @@ my $proto  = LOG_UDP;
 
 if ($ARGV[0]==2) {
     $proto  = LOG_TCP;
-    @ssl    = (1, Timeout => 2,  SSL_ca_file => => "$Bin/certs/syslog.papertrail.crt"); # , SSL_cert_file => 'certs/client-cert.pem', SSL_key_file => 'certs/client-key.pem' );
+    @ssl    = (1, Timeout => 2, SSL_ca_file => "$Bin/certs/loggly_full.crt", SSL_cert_file => "$Bin/certs/client-cert.pem", SSL_key_file => "$Bin/certs/client-key.pem");
     $msg   .= " - TLS";
 } elsif ($ARGV[0]==1) {
     $proto  = LOG_TCP;
     $msg   .= " - TCP";
 } else {
-    $msg   .= " - UDP";    
+    $msg   .= " - UDP";   
 }
 
-my $logger = Log::Syslog::Fast::PP->new($proto, 'logs.papertrailapp.com', $port, LOG_LOCAL3, LOG_CRIT, hostname, "papertrail_logger", @ssl);
-#$logger->set_format(LOG_RFC5424);
+my $logger = Log::Syslog::Fast::PP->new($proto, 'logs.loggly.com', $port, LOG_LOCAL3, LOG_CRIT, hostname, "loggly_logger", @ssl);
 print "Sent: ".$logger->send($msg, time)."\n";
